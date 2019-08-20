169a964989ef56abcaae7ae6e3273a07104d5f9a
cs671660 <christian.scheiderer@ima-zlw-ifu.rwth-aachen.de>
2019-08-20 14:16:53 +0200

    Some adjustments required to resolve merge conflict

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template, flash, url_for, redirect, request, jsonify
-import utils
+from flask import Flask, render_template, flash, url_for, redirect, request, \
+    jsonify
+from utils.generator import DataGenerator
 import os
 import numpy as np
+import time
+from sklearn.tree import DecisionTreeRegressor
+from utils.tree_export import tree_to_json
+
 
 PICTURE_FOLDER = os.path.join('static', 'pictures')
 
@@ -9,7 +14,8 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['UPLOAD_FOLDER'] = PICTURE_FOLDER
 
-data_generator = utils.DataGenerator()
+data_generator = DataGenerator()
+
 
 @app.route('/')
 @app.route('/intro/')
@@ -23,11 +29,9 @@ def acquire_data():
 
     # print(df_X.shape, df_y.shape)
 
-    headers = df_X.columns + df_y.columns
+    headers = np.hstack((df_X.columns, df_y.columns))
 
     table = np.hstack((df_X.values, df_y.values))
-
-    # print(table.shape)
 
     n_samples, n_features = df_X.shape
 
@@ -54,92 +58,52 @@ def preprocessing():
 
     return render_template('preprocessing.html', table=table,
                            headers=headers, n_samples=n_samples,
-                           n_features=n_features, strategy=strategy, means=means)
+                           n_features=n_features, strategy=strategy,
+                           means=means)
 
 
 @app.route('/training/', methods=['GET', 'POST'])
 def training():
-    """WTForms does not supply a good form with sliders. Therefore a different structure with html and JavaScript
-    sliders is being used for the inputs here."""
 
-    if request.method == 'POST':
-        flash('Successfully trained DecisionTreeRegressor.', 'success')
+    if all(k in request.form for k in ['max_depth', 'min_samples_leaf', 'max_features']):
+
         max_depth = int(request.form['max_depth'])
-        min_samples_leaf = int(request.form['min_samples_leaf'])
         max_features = float(request.form['max_features'])
+        min_samples_leaf = int(request.form['min_samples_leaf'])
 
-        utils.training(max_features, min_samples_leaf, max_depth)
-
-        return redirect(url_for('training'))
-
-    # if data has not been generated before
-    if any(i is None for i in utils.get_data('raw_data')):
-        flash('Please get dataset before continuing.', 'danger')
-        return redirect(url_for('acquire_data'))
-
-    # if data has not been preprocessed before
-    elif any(i is None for i in utils.get_data('preprocessed_data')):
-        flash('Cannot train DecisionTreeRegressor without preprocessing.', 'danger')
-        return redirect(url_for('preprocessing'))
-
-    # if the model has been trained before (check if the real_training_score is None)
-    elif utils.get_data('training_data')[0] is not None:
-        real_training_score = utils.get_scores()['real_training_score']
-        real_test_score = utils.get_scores()['real_test_score']
-        n_samples = len(utils.get_table('preprocessed_data')[0])
-
-        max_features, min_samples_leaf, max_depth = utils.get_data('training_data')[2:]
-
-        full_filename = utils.get_train_filename()
-
-        return render_template('training.html', title='Training', full_filename=full_filename,
-                               training_score=real_training_score, test_score=real_test_score, n_samples=n_samples,
-                               max_features=max_features, min_samples_leaf=min_samples_leaf, max_depth=max_depth)
-
-    # if the model is to be trained next
     else:
-        n_samples = len(utils.get_table('preprocessed_data')[0])
-        return render_template('training.html', title='Training', n_samples=n_samples)
+        max_depth = None
+        max_features = None
+        min_samples_leaf = 1
+
+    regressor = DecisionTreeRegressor(max_features=max_features,
+                              min_samples_leaf=min_samples_leaf,
+                              max_depth=max_depth)
+
+
+
+    df_X, df_y = data_generator.get_preprocessed_data()
+
+    # regressor = regressor.fit(df_X, df_y)
+
+    # json_data = tree_to_json(regressor)
+
+    json_data = {"error": 42716.2954, "samples": 506, "value": [22.532806324110698],
+            "label": "RM <= 6.94", "type": "split", "children": [
+            {"error": 17317.3210, "samples": 430, "value": [19.93372093023257],
+             "label": "LSTAT <= 14.40", "type": "leaf"},
+            {"error": 6059.4193, "samples": 76, "value": [37.23815789473684],
+             "label": "RM <= 7.44", "type": "leaf"}]}
+
+    import json
+
+    return render_template('training.html', tree_data=json_data)
 
 
 @app.route('/deployment/', methods=['GET', 'POST'])
 def deployment():
 
-    if request.method == 'POST':
-        flash('Predicted sample with given feature values.', 'success')
-        sliders = utils.get_slider_config()
-        sample = []
-        for slider in sliders:
-            slider_name = slider['name']
-            feature_val = float(request.form[slider_name])
-            sample.append(feature_val)
-
-        utils.deploy(sample)
-
-        return redirect(url_for('deployment'))
-
-    # if data has not been generated before
-    if any(i is None for i in utils.get_data('raw_data')):
-        flash('Please get dataset before continuing.', 'danger')
-        return redirect(url_for('acquire_data'))
-
-    # if data has not been preprocessed before
-    elif any(i is None for i in utils.get_data('preprocessed_data')):
-        flash('Cannot train DecisionTreeRegressor without preprocessing.', 'danger')
-        return redirect(url_for('preprocessing'))
-
-    # if the model has not been trained before
-    elif utils.get_data('training_data')[0] is None:
-        flash('You need to train the DecisionTreeRegressor first.', 'danger')
-        return redirect(url_for('training'))
-
-    else:
-        sliders = utils.get_slider_config()
-        # sample is a list of tuples
-        sample, prediction = utils.get_sample_pred()
-        filename = utils.get_deploy_filename()
-        return render_template('deployment.html', title='Deployment', sliders=sliders,
-                               sample=sample, prediction=prediction, filename=filename)
+    return render_template('deployment.html')
 
 
 if __name__ == '__main__':
