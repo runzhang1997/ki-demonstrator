5bfd535184d304407f56ce02de556d4db3c1cacc
Run Zhang <z.run@wzl.rwth-aachen.de>
2019-10-02 11:55:28 +0200

    fix deployment tree

--- app.py
+++ app.py
@@ -167,5 +167,5 @@ def deployment():
 
 
 if __name__ == '__main__':
-    #app.run(host="127.0.0.1", port=80)
-    app.run(debug=True)
+    app.run(host="127.0.0.1", port=80)
+    #app.run(debug=True)
